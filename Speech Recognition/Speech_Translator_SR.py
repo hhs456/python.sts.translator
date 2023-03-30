6d2283cfa3339b59a9a879fb0a9aabaf164dfe8a
Hanson <75490747+hhs456@users.noreply.github.com>
2023-03-30 23:40:49 +0800

    Refator
    Add speech to speech translator code using Google Translate API for Chinese and English languages.

--- Speech Recognition/Speech_Translator_SR.py
+++ Speech Recognition/Speech_Translator_SR.py
@@ -1,8 +1,6 @@
-# coding: utf-8
+# Required Libraries
 import speech_recognition as sr
 from googletrans import Translator
-import pyaudio
-import os
 import pyttsx3
 
 # Google Translate setup
@@ -12,60 +10,34 @@ translator = Translator()
 r = sr.Recognizer()
 mic = sr.Microphone()
 
-
-# Define audio recording and transcription functions
+# Define audio recording and transcription function
 def record_audio():
-    audio = pyaudio.PyAudio()
-    stream = audio.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True,
-                        frames_per_buffer=1024)
-
-    print('Recording...')
-    frames = []
-    for i in range(0, int(16000 / 1024 * 5)):
-        data = stream.read(1024)
-        frames.append(data)
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
-
-    return b''.join(frames)
-
-
-# Define function to play audio
-def play_audio(audio_path):
-    os.system("afplay " + audio_path)
-
+    with mic as source:
+        print("Please speak now: ")
+        audio_data = r.listen(source)
+    return audio_data
 
 # Usage
-with mic as source:
-    print("Please speak now: ")
-    audio_data = r.listen(source)
-
 try:
+    # Record audio and convert to text
+    audio_data = record_audio()
     text = r.recognize_google(audio_data, language='zh-TW')
     print("Text:", text)
+    
+    # Translate the text into English
     translation = translator.translate(text, src='zh-tw', dest='en')
     print("Translation:", translation.text)
     
-    # Convert translated text to speech and save to file
+    # Convert the translated text and source text into speech and save the audio files
     speaker = pyttsx3.init()
     speaker.setProperty('voice', 'zh')
     speaker.say(text)
     speaker.setProperty('voice', 'en')
     speaker.say(translation.text)    
     
-    # Save the audio file
+    # Save the audio files
     speaker.save_to_file(text, r'D:\User\Documents\GitHub\Speech-Translator\Speaker\chinese.mp3')    
     speaker.save_to_file(translation.text, r'D:\User\Documents\GitHub\Speech-Translator\Speaker\english.mp3')
+    speaker.runAndWait()    
     
-    # Run
-    speaker.runAndWait()        
-    
-except Exception as e:
-    print("Error:", e)
-
-audio_data = record_audio()
-with sr.AudioFile(audio_data) as source:
-    audio = r.record(source)
-    text = r.recognize_google(audio, language='zh-TW')
-    print('Transcript:', text)
+except Exception as e:    print("Error:", e)
